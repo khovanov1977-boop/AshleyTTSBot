36245e0967a83c228c0cb37523f4183e61208e04
Alexey <khovanov1977@gmail.com>
2026-05-26 15:52:01 +0300

    add edge tts

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import asyncio
 import logging
 import os
 
+import edge_tts
+
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import FSInputFile, Message
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,17 +18,33 @@ dp = Dispatcher()
 
 logging.basicConfig(level=logging.INFO)
 
+VOICE = "ru-RU-SvetlanaNeural"
+
 
 @dp.message(CommandStart())
 async def start_handler(message: Message):
-    await message.answer("Бот работает.")
+    await message.answer(
+        "Отправьте текст, и я озвучу его."
+    )
 
 
 @dp.message()
-async def echo_handler(message: Message):
-    await message.answer(
-        f"Вы написали:\n{message.text}"
+async def tts_handler(message: Message):
+
+    text = message.text
+
+    file_name = "voice.mp3"
+
+    communicate = edge_tts.Communicate(
+        text=text,
+        voice=VOICE
     )
+
+    await communicate.save(file_name)
+
+    audio = FSInputFile(file_name)
+
+    await message.answer_voice(audio)
 
 
 async def main():
